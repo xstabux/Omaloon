30505a71b75402acba191bef137bc8e86a3232f5
xStaBUx <buksta@outlook.com>
2022-12-11 17:52:45 +0200

    stats-cleanup

--- src/ol/world/meta/OlStat.java
+++ src/ol/world/meta/OlStat.java
@@ -1,15 +1,8 @@
 package ol.world.meta;
 
-import arc.*;
-import arc.struct.*;
-import mindustry.world.meta.Stat;
-import mindustry.world.meta.StatCat;
-import mindustry.world.meta.StatUnit;
+import mindustry.world.meta.*;
 
-import java.util.*;
-
-public class OlStat extends Stat {
-    public static final Seq<Stat> all = new Seq<>();
+public class OlStat {
 
     public static final Stat
     magnetic = new Stat("magnetic"),
@@ -21,29 +14,4 @@ public class OlStat extends Stat {
     //averageSurfaceTemperature = new Stat("average-surface-temperature"),
     //rad = new Stat("radius"),
     //orbitRad = new Stat("orbit-radius");
-
-    public final StatCat category;
-    public final String name;
-    public final int id;
-
-    public OlStat(String name, StatCat category){
-        super(name);
-        this.category = category;
-        this.name = name;
-        id = all.size;
-        all.add(this);
-    }
-
-    public OlStat(String name){
-        this(name, StatCat.general);
-    }
-
-    public String localized(){
-        return Core.bundle.get("stat." + name.toLowerCase(Locale.ROOT));
-    }
-
-    @Override
-    public int compareTo(Stat o){
-        return id - o.id;
-    }
 }
--- src/ol/world/meta/OlStatCat.java
+++ src/ol/world/meta/OlStatCat.java
@@ -1,32 +1,9 @@
 package ol.world.meta;
 
-import arc.Core;
-import arc.struct.Seq;
-import mindustry.world.meta.StatCat;
+import mindustry.world.meta.*;
 
-public class OlStatCat extends StatCat {
-    public static final Seq<StatCat> all = new Seq<>();
+public class OlStatCat {
 
     public static final StatCat
-
     pressure = new StatCat("pressure");
-
-    public final String name;
-    public final int id;
-
-    public OlStatCat(String name){
-        super(name);
-        this.name = name;
-        id = all.size;
-        all.add(this);
-    }
-
-    public String localized(){
-        return Core.bundle.get("category." + name);
-    }
-
-    @Override
-    public int compareTo(StatCat o){
-        return id - o.id;
-    }
 }
