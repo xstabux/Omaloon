03102517a11a1df447057f908ec3f72e324f1795
xStaBUx <buksta@outlook.com>
2023-05-23 19:32:38 +0300

    update mindustryVersion

---
+++ core/src/ol/world/blocks/distribution/TubeConveyor.java
@@ -0,0 +1,10 @@
+package ol.world.blocks.distribution;
+
+import mindustry.world.blocks.distribution.*;
+
+public class TubeConveyor extends Conveyor {
+
+    public TubeConveyor(String name) {
+        super(name);
+    }
+}
