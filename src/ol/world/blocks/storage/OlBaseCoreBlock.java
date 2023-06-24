88b188f4c20541924a1898908c5233118ff34df0
TheEE145 <Arhitector2009@gmail.com>
2023-06-24 19:34:58 +0300

    Update OlBaseCoreBlock.java

--- src/ol/world/blocks/storage/OlBaseCoreBlock.java
+++ src/ol/world/blocks/storage/OlBaseCoreBlock.java
@@ -1,8 +1,13 @@
 package ol.world.blocks.storage;
 
+import arc.graphics.g2d.Draw;
+import arc.util.Time;
 import mindustry.Vars;
 import mindustry.gen.Minerc;
+import mindustry.graphics.Drawf;
+import mindustry.graphics.Layer;
 import mindustry.world.blocks.storage.*;
+import mindustry.world.blocks.units.UnitFactory;
 import ol.content.OlItems;
 import ol.content.OlUnitTypes;
 
@@ -37,15 +42,38 @@ public class OlBaseCoreBlock extends CoreBlock {
     }
 
     public class OlBaseCoreBuild extends CoreBuild {
+        public int tmp = 0;
+
+        @Override
+        public void draw() {
+            super.draw();
+            switch(type) {
+                case LANDING_CAPSULE -> {
+                    if(tmp > 0) {
+                        Draw.draw(Layer.blockBuilding, () -> {
+                            Drawf.construct(this, OlUnitTypes.drillUnit, 0, tmp/(60f * 4f), 1, Time.globalTime);
+                        });
+                    }
+                }
+                default -> {
+                    //nothing
+                }
+            }
+        }
+
         @Override
         public void updateTile() {
             super.updateTile();
             switch(type) {
                 case LANDING_CAPSULE -> {
                     if(items().get(OlItems.grumon) >= 200 && hasAccessibleUnloadPoint()) {
-                        items().remove(OlItems.grumon, 200);
-                        OlUnitTypes.drillUnit.spawn(team, this);
-                        //TODO unit spawn fx
+                        if(tmp++ >= 60 * 4) {
+                            items().remove(OlItems.grumon, 200);
+                            OlUnitTypes.drillUnit.spawn(team, this);
+                            //TODO unit spawn fx
+                        }
+                    } else {
+                        tmp = 0;
                     }
                 }
                 default -> {
