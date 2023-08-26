2546c70cc429b6c50fc9cd62e0ede69a5d14e42d
Zelaux <ura20060605@gmail.com>
2023-08-26 23:06:11 +0300

    Added Draw3dSpin

--- main/src/omaloon/content/blocks/OlPowerBlocks.java
+++ main/src/omaloon/content/blocks/OlPowerBlocks.java
@@ -1,20 +1,35 @@
 package omaloon.content.blocks;
 
+import arc.math.geom.*;
+import mindustry.*;
 import mindustry.type.*;
 import mindustry.world.*;
+import mindustry.world.draw.*;
 import omaloon.world.blocks.power.*;
+import omaloon.world.draw.*;
 
-import static mindustry.type.ItemStack.*;
+import static mindustry.type.ItemStack.empty;
 
-public class OlPowerBlocks {
+public class OlPowerBlocks{
     public static Block
-            windTurbine,
+    windTurbine,
 
     end;
 
     public static void load(){
         windTurbine = new WindGenerator("wind-turbine"){{
             requirements(Category.power, empty);
+            drawer = new DrawMulti(
+            new DrawDefault(),
+            new Draw3dSpin(){{
+                suffix="-rotator";
+                regionOffset.x = Vars.tilesize / 6f;
+                transformation.idt();
+                transformation.rotate(Vec3.Y,-75);
+                transformation.scale(0.5f,1f,1f);
+            }}
+
+            );
             size = 1;
             powerProduction = 0.2f;
         }};
