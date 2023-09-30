ca5a6e60f804295883bf96134d84bc187cd27170
Zelaux <ura20060605@gmail.com>
2023-09-30 22:25:19 +0300

    Fixed broken draw ordering by Draw3dSpin

--- main/src/omaloon/world/draw/Draw3dSpin.java
+++ main/src/omaloon/world/draw/Draw3dSpin.java
@@ -7,8 +7,11 @@ import arc.graphics.g2d.*;
 import arc.graphics.gl.FrameBuffer;
 import arc.math.*;
 import arc.math.geom.*;
+import arc.struct.*;
 import arc.util.*;
 import arclibrary.graphics.*;
+import mindustry.*;
+import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
 import mindustry.world.*;
@@ -17,13 +20,22 @@ import mindustry.world.draw.*;
 import static arc.Core.*;
 
 @SuppressWarnings("UnusedReturnValue")
-public class Draw3dSpin extends DrawBlock {
+public class Draw3dSpin extends DrawBlock{
     protected static final Mat3D transformation = new Mat3D();
+    protected static final FloatSeq transformationQueue = new FloatSeq();
     protected static final Vec2 tmpPixelOffset = new Vec2();
     protected static final Vec2 tmpRotatedBaseOffset = new Vec2();
     private static final Quat tmpQuat1 = new Quat();
     private static final Quat tmpQuat2 = new Quat();
     private static final Mat3D tmpMat1 = new Mat3D();
+    private static FrameBuffer shadowBuffer;
+
+    static{
+        Events.run(Trigger.postDraw, () -> {
+            transformationQueue.size = 0;
+        });
+    }
+
     public final Vec2 baseOffset = new Vec2();
     public final Vec3 scale = new Vec3(1, 1, 1);
     public float rotateSpeed = 4f;
@@ -35,119 +47,131 @@ public class Draw3dSpin extends DrawBlock {
     public int regionWidth = 6;
     public String suffix = "";
     public String holderSuffix = "";
+    public int shadowElevation = Vars.tilesize;
     protected TextureRegion region, holderRegion;
-    private static FrameBuffer shadowBuffer;
 
-    private static void setScale(float[] val, float x, float y, float z) {
+    private static void setScale(float[] val, float x, float y, float z){
         val[0] = x;
         val[5] = y;
         val[10] = z;
         val[15] = 1;
-        for (int i = 0; i < 4; i++) {
+        for(int i = 0; i < 4; i++){
             val[i + 1] = 0;
             val[i + 6] = 0;
             val[i + 11] = 0;
         }
     }
 
-    public float baseRotation(Building building) {
+    public float baseRotation(Building building){
         return rotationProvider == null ? 0f : (360 - (rotationProvider.get(building) % 360)) % 360f;
     }
 
     @SuppressWarnings("unchecked")
-    public <T extends Building> Draw3dSpin rotationProvider(Floatf<T> rotationProvider) {
-        this.rotationProvider = (Floatf<Building>) rotationProvider;
+    public <T extends Building> Draw3dSpin rotationProvider(Floatf<T> rotationProvider){
+        this.rotationProvider = (Floatf<Building>)rotationProvider;
         return this;
     }
 
-    public TextureRegion region() {
+    public TextureRegion region(){
         return region;
     }
 
     @Override
-    public void load(Block block) {
+    public void load(Block block){
         super.load(block);
         region = Core.atlas.find(block.name + suffix);
         holderRegion = Core.atlas.find(block.name + holderSuffix);
     }
 
     @Override
-    public void draw(Building build) {
-        super.draw(build);
-        float realWidth = region.width * region.scl() * Draw.xscl;
-        float realHeight = region.height * region.scl() * Draw.yscl;
-        float baseRotation = baseRotation(build);
+    public void draw(Building build){
+        final float startZ = Draw.z();
+        try{
+            float realWidth = region.width * region.scl() * Draw.xscl;
+            float realHeight = region.height * region.scl() * Draw.yscl;
+            float baseRotation = baseRotation(build);
 
-        float time = build.totalProgress() * rotateSpeed;
-        float mainRotation = time % 90 + startRotationOffset;
-        float subRotation = time % 90 - 90 + startRotationOffset;
-        Mat baseRotationMatrix;
+            float time = build.totalProgress() * rotateSpeed;
+            float mainRotation = time % 90 + startRotationOffset;
+            float subRotation = time % 90 - 90 + startRotationOffset;
+            Mat baseRotationMatrix;
 
-        setupTransformations:
-        {
-            transformation.idt();
-            float baseCos = Mathf.cosDeg(baseRotation);
-            float baseSin = Mathf.sinDeg(baseRotation);
-            baseRotationMatrix = Tmp.m1.setToRotation(Vec3.Z, baseCos, baseSin);
+            setupTransformations:
+            {
+                transformation.idt();
+                float baseCos = Mathf.cosDeg(baseRotation);
+                float baseSin = Mathf.sinDeg(baseRotation);
+                baseRotationMatrix = Tmp.m1.setToRotation(Vec3.Z, baseCos, baseSin);
 
-            Vec3 tmp = Tmp.v31;
-            tmp.set(axis).mul(baseRotationMatrix);
-            tmpQuat2.setFromAxis(tmp, rotationAroundAxis);
+                Vec3 tmp = Tmp.v31;
+                tmp.set(axis).mul(baseRotationMatrix);
+                tmpQuat2.setFromAxis(tmp, rotationAroundAxis);
 
-            tmpQuat1.setFromAxis(tmpQuat2.transform(tmp.set(0, 0, 1)), baseRotation);
-            transformation.rotate(tmpQuat2);
-            tmp.set(1f, 1f, 1f).sub(scale).mul(baseRotationMatrix);
-            Mat3D mat3D = tmpMat1;
-            setScale(mat3D.val, 1f - tmp.x * tmp.x, 1f - tmp.y * tmp.y, 1f - tmp.z * tmp.z);
+                tmpQuat1.setFromAxis(tmpQuat2.transform(tmp.set(0, 0, 1)), baseRotation);
+                transformation.rotate(tmpQuat2);
+                tmp.set(1f, 1f, 1f).sub(scale).mul(baseRotationMatrix);
+                Mat3D mat3D = tmpMat1;
+                setScale(mat3D.val, 1f - tmp.x * tmp.x, 1f - tmp.y * tmp.y, 1f - tmp.z * tmp.z);
 
-            transformation.mul(mat3D);
-            mat3D.idt();
-            mat3D.rotate(Vec3.Z, baseRotation);
-            transformation.mul(mat3D);
-            baseRotationMatrix.scale(1f, -1f);
-        }
+                transformation.mul(mat3D);
+                mat3D.idt();
+                mat3D.rotate(Vec3.Z, baseRotation);
+                transformation.mul(mat3D);
+                baseRotationMatrix.scale(1f, -1f);
+            }
 
-        float alpha = Mathf.mod(time, 90) / 90f;
-        baseRotation = Mathf.mod(baseRotation, 180f);
-        Vec2 pixelOffset = tmpPixelOffset.set(-pixelSize, 0).mul(baseRotationMatrix);
-        Vec2 baseOffset = tmpRotatedBaseOffset.set(this.baseOffset).mul(baseRotationMatrix);
+            float alpha = Mathf.mod(time, 90) / 90f;
+            baseRotation = Mathf.mod(baseRotation, 180f);
+            final float finalBaseRotation = baseRotation;
+            Vec2 pixelOffset = tmpPixelOffset.set(-pixelSize, 0).mul(baseRotationMatrix);
+            Vec2 baseOffset = tmpRotatedBaseOffset.set(this.baseOffset).mul(baseRotationMatrix);
 
-        int halfRegionWidth = regionWidth / 2;
-        float drawX = build.x + baseOffset.x + pixelOffset.x * halfRegionWidth - realWidth / 2f;
-        float drawY = build.y + baseOffset.y + pixelOffset.y * halfRegionWidth - realHeight / 2f;
+            int halfRegionWidth = regionWidth / 2;
+            float drawX = build.x + baseOffset.x + pixelOffset.x * halfRegionWidth - realWidth / 2f;
+            float drawY = build.y + baseOffset.y + pixelOffset.y * halfRegionWidth - realHeight / 2f;
 
-        if (shadowBuffer == null) {
-            shadowBuffer = new FrameBuffer(Pixmap.Format.rgba8888, region.width, region.height, false);
-        }
+            shadows:
+            {
+                int myIndex = transformationQueue.size;
+                transformationQueue.addAll(transformation.val);
+                Draw.draw(Layer.blockProp + 1, () -> {
+                    if(shadowBuffer == null){
+                        shadowBuffer = new FrameBuffer(graphics.getWidth(), graphics.getHeight());
+                    }
+                    Draw.flush();
+                    shadowBuffer.resize(graphics.getWidth(), graphics.getHeight());
+                    shadowBuffer.begin(Color.clear);
+                    Draw.color();
 
-        shadowBuffer.resize(graphics.getWidth(), graphics.getHeight());
-        shadowBuffer.begin(Color.clear);
-        Draw.color(Color.white);
-        Draw3d.rect(transformation, region, drawX - 8, drawY - 8, realWidth, realHeight, mainRotation);
-        Lines.stroke(2);
-        Draw.rect(holderRegion, build.x - 8, build.y - 8, -baseRotation);
-        Lines.line(build.x, build.y, build.x - 8, build.y - 8);
-        Draw.color();
-        shadowBuffer.end();
-        Draw.color(Pal.shadow, Pal.shadow.a);
-        Draw.z(Layer.blockProp + 1);
-        Draw.rect(Draw.wrap(shadowBuffer.getTexture()), camera.position.x, camera.position.y, camera.width, -camera.height);
-        Draw.color();
-
-        float a = Draw.getColor().a;
-        Draw.z(Layer.blockOver);
-        Draw.rect(holderRegion, build.x, build.y, -baseRotation);
-        Draw.alpha(baseRotation / 180f * a);
-        Draw.rect(holderRegion, build.x, build.y, -baseRotation - 180f);
-        Draw.alpha(a);
-
-        for (int i = halfRegionWidth; i >= -halfRegionWidth; i--) {
-            Draw.alpha(1f);
-            Draw3d.rect(transformation, region, drawX, drawY, realWidth, realHeight, mainRotation);
-            Draw.alpha(alpha);
-            Draw3d.rect(transformation, region, drawX, drawY, realWidth, realHeight, subRotation);
-            drawX -= pixelOffset.x;
-            drawY -= pixelOffset.y;
+                    System.arraycopy(transformationQueue.items, myIndex, transformation.val, 0, transformation.val.length);
+                    Draw3d.rect(transformation, region, drawX - shadowElevation, drawY - shadowElevation, realWidth, realHeight, mainRotation);
+                    Lines.stroke(2);
+                    Draw.rect(holderRegion, build.x - shadowElevation, build.y - shadowElevation, -finalBaseRotation);
+                    Lines.line(build.x, build.y, build.x - shadowElevation, build.y - shadowElevation);
+                    Draw.color();
+                    shadowBuffer.end();
+                    Draw.color(Pal.shadow, Pal.shadow.a);
+                    Draw.rect(Draw.wrap(shadowBuffer.getTexture()), camera.position.x, camera.position.y, camera.width, -camera.height);
+                    Draw.flush();
+                    Draw.color();
+                });
+            }
+            Draw.z(Layer.blockProp + 2);
+            float a = Draw.getColor().a;
+            Draw.rect(holderRegion, build.x, build.y, -finalBaseRotation);
+            Draw.alpha(finalBaseRotation / 180f * a);
+            Draw.rect(holderRegion, build.x, build.y, -finalBaseRotation - 180f);
+            float localDrawX = drawX, localDrawY = drawY;
+            for(int i = halfRegionWidth; i >= -halfRegionWidth; i--){
+                Draw.alpha(1f);
+                Draw3d.rect(transformation, region, localDrawX, localDrawY, realWidth, realHeight, mainRotation);
+                Draw.alpha(alpha);
+                Draw3d.rect(transformation, region, localDrawX, localDrawY, realWidth, realHeight, subRotation);
+                localDrawX -= pixelOffset.x;
+                localDrawY -= pixelOffset.y;
+            }
+        }finally{
+            Draw.z(startZ);
         }
     }
 }
