77e6cb9bd1a26441b98a8c699d34ddffaff090e0
xStaBUx <buksta@outlook.com>
2023-10-17 22:25:42 +0300

    TubeJunction :з

--- main/src/omaloon/content/blocks/OlDistributionBlocks.java
+++ main/src/omaloon/content/blocks/OlDistributionBlocks.java
@@ -1,7 +1,9 @@
 package omaloon.content.blocks;
 
+import mindustry.graphics.Layer;
 import mindustry.type.*;
 import mindustry.world.*;
+import mindustry.world.draw.*;
 import mindustry.world.meta.*;
 
 import omaloon.world.blocks.distribution.*;
@@ -10,7 +12,7 @@ import static mindustry.type.ItemStack.*;
 
 public class OlDistributionBlocks {
     public static Block
-            tubeConveyor,
+            tubeConveyor, tubeJunction,
 
         end;
 
@@ -20,6 +22,19 @@ public class OlDistributionBlocks {
             health = 65;
             speed = 0.03f;
             displayedSpeed = 4.2f;
+        }};
+
+        tubeJunction = new TubeJunction("tube-junction"){{
+            requirements(Category.distribution, BuildVisibility.sandboxOnly, with());
+            speed = 24;
+            capacity = 4;
+            health = 30;
+            drawer = new DrawMulti(
+                    new DrawRegion("-bottom"){{
+                        layer = Layer.blockUnder;
+                    }},
+                    new DrawDefault()
+            );
         }};
     }
 }
