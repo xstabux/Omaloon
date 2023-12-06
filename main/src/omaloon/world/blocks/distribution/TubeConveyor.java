97cd2fb03ceb614e58e043f64d77ec94c5960204
xStaBUx <buksta@outlook.com>
2023-12-06 22:18:38 +0200

    no bugs

--- main/src/omaloon/world/blocks/distribution/TubeConveyor.java
+++ main/src/omaloon/world/blocks/distribution/TubeConveyor.java
@@ -1,5 +1,6 @@
 package omaloon.world.blocks.distribution;
 
+import arc.Events;
 import arc.func.*;
 import arc.graphics.g2d.*;
 import arc.math.*;
@@ -8,6 +9,7 @@ import arc.struct.*;
 import arc.util.*;
 
 import mindustry.entities.units.*;
+import mindustry.game.EventType;
 import mindustry.gen.*;
 import mindustry.graphics.*;
 import mindustry.type.Item;
@@ -22,9 +24,9 @@ import static mindustry.Vars.*;
 import static omaloon.utils.OlUtils.*;
 
 //TODO topRegion for planning
-public class TubeConveyor extends Conveyor {
+public class TubeConveyor extends Conveyor{
     private static final float itemSpace = 0.4f;
-    public static final int[][] tiles = new int[][] {
+    public static final int[][] tiles = new int[][]{
             {},
             {0, 2}, {1, 3}, {0, 1},
             {0, 2}, {0, 2}, {1, 2},
@@ -37,19 +39,19 @@ public class TubeConveyor extends Conveyor {
     public TextureRegion[] capRegion;
     public Block junctionReplacement;
 
-    public TubeConveyor(String name) {
+    public TubeConveyor(String name){
         super(name);
     }
 
     @Override
-    public void init() {
+    public void init(){
         super.init();
 
         if(junctionReplacement == null) junctionReplacement = OlDistributionBlocks.tubeJunction;
     }
 
     @Override
-    public Block getReplacement(BuildPlan req, Seq<BuildPlan> plans) {
+    public Block getReplacement(BuildPlan req, Seq<BuildPlan> plans){
         if (junctionReplacement == null) return this;
 
         Boolf<Point2> cont = p -> plans.contains(o -> o.x == req.x + p.x && o.y == req.y + p.y
@@ -64,20 +66,21 @@ public class TubeConveyor extends Conveyor {
     }
 
     @Override
-    public void drawPlanRegion(BuildPlan plan, Eachable<BuildPlan> list) {
+    public void drawPlanRegion(BuildPlan plan, Eachable<BuildPlan> list){
         super.drawPlanRegion(plan, list);
     }
 
     @Override
-    public void load() {
+    public void load(){
         super.load();
         topRegion = OlUtils.splitLayers(name + "-sheet", 32, 2);
         capRegion = new TextureRegion[] { topRegion[1][0], topRegion[1][1] };
         uiIcon = atlas.find(name + "-icon");
     }
 
-    public class TubeConveyorBuild extends ConveyorBuild {
+    public class TubeConveyorBuild extends ConveyorBuild{
         public int tiling = 0;
+        public int calls = 0;
 
         @Override
         public void updateTile(){
@@ -97,20 +100,20 @@ public class TubeConveyor extends Conveyor {
                 nextMax = Math.min(nextMax, 1f - itemSpace);
             }
 
-            if (isEnd(reverse(rotation)) && blendbits == 0) {
+            if(isEnd(reverse(rotation)) && blendbits == 0){
                 float nextMaxReverse = aligned ? (items.total() > 2 ? (0.5f - Math.max(itemSpace - nextc.minitem, 0))
                         : Math.max(itemSpace - nextc.minitem, 0)) : 0f;
 
                 float movedReverse = speed * edelta();
 
-                for (int i = 0; i < len; i++) {
+                for(int i = 0; i < len; i++){
                     float nextposReverse = (i == 0 ? 0f : ys[i - 1]) + itemSpace;
                     float maxmoveReverse = Mathf.clamp(ys[i] - nextposReverse, 0, movedReverse);
 
                     ys[i] += maxmoveReverse;
 
-                    if (ys[i] < nextMaxReverse) ys[i] = nextMaxReverse;
-                    if (ys[i] < minitem) minitem = ys[i];
+                    if(ys[i] < nextMaxReverse) ys[i] = nextMaxReverse;
+                    if(ys[i] < minitem) minitem = ys[i];
                 }
             }
 
@@ -125,6 +128,12 @@ public class TubeConveyor extends Conveyor {
                 if(ys[i] > nextMax) ys[i] = nextMax;
                 if(ys[i] > 0.5 && i > 0) mid = i - 1;
                 xs[i] = Mathf.approach(xs[i], 0, moved * 2);
+
+                if(isEnd(rotation) && isEnd(reverse(rotation)) && items.total() > 1 && calls > 1){
+                    //remove last item
+                    items.remove(ids[i], len - i);
+                    len = Math.min(i, len);
+                }
 
                 if(ys[i] >= 1f && pass(ids[i])){
                     // align X position if passing forwards
@@ -150,6 +159,11 @@ public class TubeConveyor extends Conveyor {
             noSleep();
         }
 
+        public void updateProximity() {
+            super.updateProximity();
+            calls++;
+        }
+
         public Building buildAt(int i){
             return nearby(i);
         }
@@ -166,7 +180,7 @@ public class TubeConveyor extends Conveyor {
         }
 
         @Override
-        public void draw() {
+        public void draw(){
             int frame = enabled && clogHeat <= 0.5f ? (int) (((Time.time * speed * 8f * timeScale * efficiency)) % 4) : 0;
 
             //draw extra conveyors facing this one for non-square tiling purposes
@@ -220,13 +234,14 @@ public class TubeConveyor extends Conveyor {
         }
 
         @Override
-        public int acceptStack(Item item, int amount, Teamc source) {
-            if (isEnd(reverse(rotation)) && items.total() >= 2) return 0;
+        public int acceptStack(Item item, int amount, Teamc source){
+            if(isEnd(reverse(rotation)) && items.total() >= 2) return 0;
+            if(isEnd(reverse(rotation)) && isEnd(rotation) && items.total() >= 1) return 0;
             return Math.min((int)(minitem / itemSpace), amount);
         }
 
         @Override
-        public void unitOn(Unit unit) {
+        public void unitOn(Unit unit){
         }
 
         @Override
