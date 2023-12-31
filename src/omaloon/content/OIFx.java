4cc32930afbf91ff04f329adc169a9a08c09a02d
xStaBUx <buksta@outlook.com>
2023-12-31 16:05:27 +0200

    several improvements

--- src/omaloon/content/OIFx.java
+++ src/omaloon/content/OIFx.java
@@ -1,18 +1,30 @@
 package omaloon.content;
 
+import arc.graphics.Color;
 import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.Fill;
+import arc.graphics.g2d.Lines;
 import arc.math.Interp;
 import arc.math.Mathf;
+import arc.math.Rand;
 import arc.util.Tmp;
 import mindustry.entities.Effect;
 import mindustry.graphics.Drawf;
 import mindustry.graphics.Layer;
+import mindustry.graphics.Pal;
 import omaloon.entities.bullet.HailStoneBulletType;
 
-public class OIFx {
-    public static Effect
+import static arc.graphics.Color.valueOf;
+import static arc.graphics.g2d.Draw.alpha;
+import static arc.graphics.g2d.Draw.color;
+import static arc.graphics.g2d.Lines.lineAngle;
+import static arc.graphics.g2d.Lines.stroke;
+import static arc.math.Angles.randLenVectors;
 
-    falledStone = new Effect(120f, e -> {
+public class OIFx {
+    public static final Rand rand = new Rand();
+    public static Effect
+    fallenStone = new Effect(120f, e -> {
         if(!(e.data instanceof HailStoneBulletType.HailStoneData data)) return;
 
         Tmp.v2.trns(Mathf.randomSeed(e.id) * 360, data.fallTime/2 + Mathf.randomSeed(e.id + 1) * data.fallTime);
@@ -28,5 +40,4 @@ public class OIFx {
         Draw.z(Layer.power + 0.2f);
         Draw.rect(data.region, x, y + (scl * data.fallTime/2), rot);
     });
-
 }
