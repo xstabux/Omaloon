b0aa878ac2d650d597cc3148a9c354128e2babe1
= <uujuju41@gmail.com>
2024-02-04 22:04:31 -0300

    surely this breaks in multiplayer

---
+++ src/omaloon/world/blocks/production/OlGenericCrafter.java
@@ -0,0 +1,13 @@
+package omaloon.world.blocks.production;
+
+import mindustry.world.blocks.production.*;
+
+public class OlGenericCrafter extends GenericCrafter {
+	public OlGenericCrafter(String name) {
+		super(name);
+	}
+
+	public class OlGenericCrafterBuild extends GenericCrafterBuild {
+
+	}
+}
