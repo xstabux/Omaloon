bc4962f5b1ec48542bf81ee943174fe5194b3169
= <uujuju41@gmail.com>
2024-03-04 15:10:54 -0300

    slightly better naming

--- src/omaloon/world/blocks/defense/Deflector.java
+++ src/omaloon/world/blocks/defense/Deflector.java
@@ -5,10 +5,12 @@ import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arc.math.*;
 import arc.scene.ui.layout.*;
+import arc.util.*;
 import arc.util.io.*;
 import mindustry.game.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
+import mindustry.ui.*;
 import mindustry.world.*;
 import mindustry.world.meta.*;
 import omaloon.world.interfaces.*;
@@ -22,12 +24,12 @@ public class Deflector extends Block {
 
 	public int rotations = 8;
 	public float deflectAlpha = 0.2f;
-	public float deflectAngle = 120f;
-	public float deflectHealth = 120f;
-	public float deflectRange = 80f;
-	public float deflectRechargeStandard = 0.1f;
-	public float deflectRechargeBroken = 1f;
-	public float deflectWarmup = 0.1f;
+	public float shieldAngle = 120f;
+	public float shieldHealth = 120f;
+	public float shieldRange = 80f;
+	public float rechargeStandard = 0.1f;
+	public float rechargeBroken = 1f;
+	public float warmupTime = 0.1f;
 	public Color deflectColor = Pal.heal;
 
 	public Deflector(String name) {
@@ -35,10 +37,13 @@ public class Deflector extends Block {
 		update = true;
 		solid = true;
 		configurable = true;
+		saveConfig = true;
 		hasLiquids = true;
 		group = BlockGroup.projectors;
 		ambientSound = Sounds.shield;
 		ambientSoundVolume = 0.08f;
+		config(Integer.class, (build, rot) -> ((DeflectorBuild) build).rot = (rot + 8) % 8);
+		configClear((DeflectorBuild build) -> build.rot = 0);
 	}
 
 	@Override
@@ -47,18 +52,51 @@ public class Deflector extends Block {
 		baseRegion = Core.atlas.find(name + "-base", "block-" + size);
 	}
 
+	@Override
+	public TextureRegion[] icons() {
+		return new TextureRegion[]{baseRegion, region};
+	}
+
+	@Override
+	public void setStats() {
+		super.setStats();
+		pressureConfig.addStats(stats);
+
+		stats.add(Stat.shieldHealth, shieldHealth, StatUnit.none);
+		stats.add(Stat.cooldownTime, (int) (rechargeStandard * 60f), StatUnit.perSecond);
+	}
+
+	@Override
+	public void setBars() {
+		super.setBars();
+		addBar("pressure", entity -> {
+			HasPressure build = (HasPressure) entity;
+			return new Bar(
+				() -> Core.bundle.get("pressure") + Strings.fixed(build.getPressure(), 2),
+				build::getBarColor,
+				build::getPressureMap
+			);
+		});
+		addBar("shield", (DeflectorBuild entity) -> new Bar("stat.shieldhealth", Pal.accent, () -> entity.broken ? 0f : 1 - entity.shieldDamage / shieldHealth).blink(Color.white));
+	}
+
 	public class DeflectorBuild extends Building implements HasPressure {
 		public PressureModule pressure = new PressureModule();
 
 		public int rot = 2;
-		public float damageShield = 0;
+		public float shieldDamage = 0;
 		public float warmup = 0;
 		public boolean broken = false;
 
 		@Override
 		public void buildConfiguration(Table table) {
-			table.button(Icon.undo, () -> rot = (rot + 1 + 8) % 8).size(50f);
-			table.button(Icon.redo, () -> rot = (rot - 1 + 8) % 8).size(50f);
+			table.button(Icon.undo, () -> configure(rot + 1)).size(50f);
+			table.button(Icon.redo, () -> configure(rot - 1)).size(50f);
+		}
+
+		@Override
+		public Integer config() {
+			return rot;
 		}
 
 		@Override
@@ -67,38 +105,38 @@ public class Deflector extends Block {
 			Draw.rect(region, x, y, rot * 360f/rotations - 90f);
 			Draw.color(deflectColor, deflectAlpha);
 			Draw.z(Layer.blockOver);
-			Fill.arc(x, y, deflectRange * warmup, deflectAngle/360f, -deflectAngle/2f + rot * 360f/rotations);
+			Fill.arc(x, y, shieldRange * warmup, shieldAngle/360f, -shieldAngle/2f + rot * 360f/rotations);
 		}
 
 		@Override
 		public void updateTile() {
 			updateDeath();
 			if (efficiency > 0) {
-				if (damageShield >= 0) {
-					damageShield -= edelta();
+				if (shieldDamage >= 0) {
+					shieldDamage -= edelta() * (broken ? rechargeBroken : rechargeStandard);
 				} else {
 					broken = false;
 				}
 
 				if (broken) {
-					warmup = Mathf.approachDelta(warmup, 0f, deflectWarmup);
+					warmup = Mathf.approachDelta(warmup, 0f, warmupTime);
 				} else {
-					warmup = Mathf.approachDelta(warmup, efficiency, deflectWarmup);
-					Groups.bullet.intersect(x - deflectRange, y - deflectRange, deflectRange * 2f, deflectRange * 2f, b -> {
+					warmup = Mathf.approachDelta(warmup, efficiency, warmupTime);
+					Groups.bullet.intersect(x - shieldRange, y - shieldRange, shieldRange * 2f, shieldRange * 2f, b -> {
 						if (b.team == Team.derelict) {
 							float distance = Mathf.dst(x, y, b.x, b.y);
 							float angle = Math.abs(((b.angleTo(x, y) - rot * 360f / rotations) % 360f + 360f) % 360f - 180f);
 
-							if (distance <= deflectRange && angle <= deflectAngle / 2f || distance <= size * 8) {
+							if (distance <= shieldRange * warmup && angle <= shieldAngle / 2f || distance <= size * 8) {
 								b.absorb();
-								damageShield += b.damage;
-								if (damageShield >= deflectHealth) broken = true;
+								shieldDamage += b.damage;
+								if (shieldDamage >= shieldHealth) broken = true;
 							}
 						}
 					});
 				}
 			} else {
-				warmup = Mathf.approachDelta(warmup, 0f, deflectWarmup);
+				warmup = Mathf.approachDelta(warmup, 0f, warmupTime);
 			}
 		}
 
@@ -132,7 +170,7 @@ public class Deflector extends Block {
 			super.read(read, revision);
 			pressure.read(read);
 			rot = read.i();
-			damageShield = read.f();
+			shieldDamage = read.f();
 			warmup = read.f();
 			broken = read.bool();
 		}
@@ -141,7 +179,7 @@ public class Deflector extends Block {
 			super.write(write);
 			pressure.write(write);
 			write.i(rot);
-			write.f(damageShield);
+			write.f(shieldDamage);
 			write.f(warmup);
 			write.bool(broken);
 		}
