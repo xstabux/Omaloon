91470930fa7964f77fc737c340a515fc501a7eb2
xStaBUx <buksta@outlook.com>
2024-03-27 01:48:20 +0200

    unit

--- src/omaloon/type/weather/EffectWeather.java
+++ src/omaloon/type/weather/EffectWeather.java
@@ -6,13 +6,13 @@ import mindustry.entities.Effect;
 import mindustry.gen.WeatherState;
 
 public class EffectWeather extends SpawnerWeather{
-    public Effect WeatherFx = Fx.none;
+    public Effect weatherFx = Fx.none;
     public EffectWeather(String name) {
         super(name);
     }
 
     @Override
     public void spawnAt(WeatherState state, float x, float y) {
-        WeatherFx.at(x, y, Mathf.angle(xspeed, yspeed));
+        weatherFx.at(x, y, Mathf.angle(xspeed, yspeed));
     }
 }
