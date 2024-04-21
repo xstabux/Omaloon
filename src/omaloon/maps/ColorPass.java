f0c0e94de113b07813adac29765baa2523eb3902
= <uujuju41@gmail.com>
2024-04-21 14:11:30 -0300

    add min and max height for NoiseColorPass

--- src/omaloon/maps/ColorPass.java
+++ src/omaloon/maps/ColorPass.java
@@ -35,17 +35,30 @@ public abstract class ColorPass {
 	 * @see omaloon.maps.HeightPass.NoiseHeight NoiseHeight
 	 */
 	public static class NoiseColorPass extends ColorPass {
+		/**
+		 * noise parameters
+		 */
 		public int seed;
 		public double octaves = 1.0, persistence = 1.0, scale = 1.0;
-		public float magnitude = 1, min = 0f, max = 1f;
+		public float magnitude = 1;
 		public Vec3 offset = new Vec3();
+
+		public float minNoise = 0f, maxNoise = 1f;
+		public float minHeight = -10f, maxHeight = 10f;
+
 		public Color out = Color.white;
 
 		@Override
 		public Color color(Vec3 pos, float height) {
-			pos = new Vec3(pos).add(offset);
-			float noise = Simplex.noise3d(seed, octaves, persistence, scale, pos.x, pos.y, pos.z) * magnitude;
-			if (min < noise && noise < max) return out;
+			float noise = Simplex.noise3d(
+				seed,
+				octaves, persistence, scale,
+				pos.x + offset.x, pos.y + offset.y, pos.z + offset.y
+			) * magnitude;
+			if (
+				(minNoise <= noise && noise <= maxNoise) &&
+				(minHeight <= height && height <= maxHeight)
+			) return out;
 			return null;
 		}
 	}
