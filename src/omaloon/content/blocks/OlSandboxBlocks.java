50960a14b16cddccb5e7ae24a07a00c3738bad79
= <uujuju41@gmail.com>
2024-05-26 16:46:23 -0300

    remove pressure void

--- src/omaloon/content/blocks/OlSandboxBlocks.java
+++ src/omaloon/content/blocks/OlSandboxBlocks.java
@@ -6,16 +6,18 @@ import mindustry.world.meta.*;
 import omaloon.world.blocks.sandbox.*;
 
 public class OlSandboxBlocks {
-	public static Block pressureSource, pressureVoid;
+	public static Block pressureSource/*, pressureVoid*/;
 
 	public static void load() {
 		pressureSource = new PressureLiquidSource("pressure-source") {{
 			buildVisibility = BuildVisibility.sandboxOnly;
 			category = Category.liquid;
 		}};
+		/*
 		pressureVoid = new PressureLiquidVoid("pressure-void") {{
 			buildVisibility = BuildVisibility.sandboxOnly;
 			category = Category.liquid;
 		}};
+		*/
 	}
 }
