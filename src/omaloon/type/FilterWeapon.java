aa50f248ccef4447d7506c6a1d424eb3702846a0
= <uujuju41@gmail.com>
2024-06-28 18:36:36 -0300

    icons

--- src/omaloon/type/FilterWeapon.java
+++ src/omaloon/type/FilterWeapon.java
@@ -1,6 +1,8 @@
 package omaloon.type;
 
+import arc.*;
 import arc.func.*;
+import arc.graphics.g2d.*;
 import arc.scene.ui.layout.*;
 import arc.struct.*;
 import arc.util.*;
@@ -19,10 +21,11 @@ public class FilterWeapon extends Weapon {
 	/**
 	 * separate from bulletFilter for stats
 	 */
-	public OrderedMap<BulletType, String> bullets = OrderedMap.of(Bullets.placeholder, "");
-	public Func<Unit, BulletType> bulletFilter = unit -> bullets.keys().toSeq().first();
+	public BulletType[] bullets = new BulletType[] {Bullets.placeholder};
+	public Func<Unit, BulletType> bulletFilter = unit -> bullets[0];
 // TODO i don't know how to make those icons work, and i have no clue as to why it is casting a string to BulletType
-//	public TextureRegion[] iconRegions;
+	public TextureRegion[] iconRegions;
+	public String[] icons = new String[] {""};
 
 	@Override
 	public void addStats(UnitType u, Table t) {
@@ -35,20 +38,31 @@ public class FilterWeapon extends Weapon {
 			t.add("[lightgray]" + Stat.reload.localized() + ": " + (mirror ? "2x " : "") + "[white]" + Strings.autoFixed(60f / reload * shoot.shots, 2) + " " + StatUnit.perSecond.localized());
 		}
 
-		t.table(Styles.grayPanel, weapon -> bullets.each((bullet, icon) -> weapon.table(Tex.underline, b -> {
-			b.left();
-//			if (iconRegions[bullets.orderedKeys().indexOf(bullet)].found()) b.image(iconRegions[bullets.orderedKeys().indexOf(bullet)]).padRight(10).center();
-			StatValues.ammo(ObjectMap.of(u, bullet)).display(b.add(new Table()).get());
-		}).growX().row())).margin(10f);
+		t.row();
+		t.table(Styles.grayPanel, weapon -> {
+			for(int i = 0; i < bullets.length; i++) {
+				int finalI = i;
+				weapon.table(Tex.underline, b -> {
+					b.left();
+					if (iconRegions[finalI].found()) b.image(iconRegions[finalI]).padRight(10).center();
+					StatValues.ammo(ObjectMap.of(u, bullet)).display(b.add(new Table()).get());
+				}).growX().row();
+			}
+		}).margin(10f);
 	}
 
-//	@Override
-//	public void load() {
-//		super.load();
-//		Seq<TextureRegion> icons = new Seq<>();
-//		bullets.orderedKeys().each(bullet -> icons.add(Core.atlas.find(bullets.get(bullet))));
-//		iconRegions = icons.toArray();
-//	}
+	@Override
+	public void load() {
+		super.load();
+		iconRegions = new TextureRegion[bullets.length];
+		for(int i = 0; i < iconRegions.length; i++) {
+			if (i < icons.length) {
+				iconRegions[i] = Core.atlas.find(icons[i]);
+			} else {
+				iconRegions[i] = Core.atlas.find("error");
+			}
+		}
+	}
 
 	@Override
 	protected void shoot(Unit unit, WeaponMount mount, float shootX, float shootY, float rotation) {
