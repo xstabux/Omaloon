8ccbe7f011973a675ac12abfb373a4e21ad23fa6
= <uujuju41@gmail.com>
2024-07-17 19:27:03 -0300

    untested, but the effects should appear now(still lacks construct animation)

--- src/omaloon/type/MasterUnitType.java
+++ src/omaloon/type/MasterUnitType.java
@@ -23,21 +23,21 @@ public class MasterUnitType extends GlassmoreUnitType {
 	@Override
 	public void draw(Unit unit) {
 		super.draw(unit);
-		if (unit instanceof Masterc u) drawConstruct(u);
+		if (unit instanceof Masterc) drawConstruct((Unit & Masterc) unit);
 	}
 
-	public void drawConstruct(Masterc u) {
+	public <T extends Unit & Masterc> void drawConstruct(T u) {
 		Draw.z(Layer.groundUnit - 0.1f);
 		if (!u.hasActionUnit()) {
 			Draw.draw(Draw.z(), () -> {
-				Tmp.v1.trns(u.rotation() - 90, actionOffset/3).add(u);
-				Drawf.construct(Tmp.v1.x, Tmp.v1.y, actionUnitType.fullIcon, u.rotation() - 90, u.droneConstructTime() / droneConstructTime, 1f, u.droneConstructTime());
+				Tmp.v1.trns(u.rotation - 90, actionOffset/3f).add(u);
+				Drawf.construct(Tmp.v1.x, Tmp.v1.y, actionUnitType.fullIcon, u.rotation - 90, u.droneConstructTime() / droneConstructTime, 1f, u.droneConstructTime());
 			});
 		}
 		if (!u.hasAttackUnit()) {
 			Draw.draw(Draw.z(), () -> {
-				Tmp.v1.trns(u.rotation() - 90, attackOffset/3).add(u);
-				Drawf.construct(Tmp.v1.x, Tmp.v1.y, attackUnitType.fullIcon, u.rotation() - 90, u.droneConstructTime() / droneConstructTime, 1f, u.droneConstructTime());
+				Tmp.v1.trns(u.rotation - 90, attackOffset/3f).add(u);
+				Drawf.construct(Tmp.v1.x, Tmp.v1.y, attackUnitType.fullIcon, u.rotation - 90, u.droneConstructTime() / droneConstructTime, 1f, u.droneConstructTime());
 			});
 		}
 	}
