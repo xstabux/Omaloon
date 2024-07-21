23cc5662e5c1b54b472842463536978a2808b0c8
xStaBUx <buksta@outlook.com>
2024-07-21 13:24:36 +0300

    coal generator

--- src/omaloon/content/blocks/OlPowerBlocks.java
+++ src/omaloon/content/blocks/OlPowerBlocks.java
@@ -1,9 +1,16 @@
 package omaloon.content.blocks;
 
+import arc.graphics.Color;
 import arc.math.geom.*;
 import mindustry.*;
+import mindustry.content.Fx;
+import mindustry.content.Items;
+import mindustry.gen.Sounds;
 import mindustry.type.*;
 import mindustry.world.*;
+import mindustry.world.blocks.power.ConsumeGenerator;
+import mindustry.world.consumers.ConsumeItemExplode;
+import mindustry.world.consumers.ConsumeItemFlammable;
 import mindustry.world.draw.*;
 import omaloon.world.blocks.power.*;
 import omaloon.world.draw.*;
@@ -12,7 +19,7 @@ import static mindustry.type.ItemStack.*;
 
 public class OlPowerBlocks{
     public static Block
-    windTurbine, impulseNode,
+    windTurbine, coalGenerator, impulseNode,
 
     end;
 
@@ -32,6 +39,29 @@ public class OlPowerBlocks{
             );
             size = 1;
             powerProduction = 0.2f;
+        }};
+
+        coalGenerator = new ConsumeGenerator("coal-generator"){{
+            requirements(Category.power, empty);
+            powerProduction = 1f;
+            itemDuration = 120f;
+
+            ambientSound = Sounds.smelter;
+            ambientSoundVolume = 0.03f;
+            effectChance = 0.06f;
+            generateEffect = Fx.fireSmoke;
+
+            consumeItem(Items.coal, 1);
+
+            drawer = new DrawMulti(new DrawDefault(),
+                    new DrawFlame(Color.valueOf("ffcd66")){{
+                        flameRadius = 2f;
+                        flameRadiusIn = 1f;
+                        flameRadiusScl = 4f;
+                        flameRadiusMag = 1f;
+                        flameRadiusInMag = 0.5f;
+                    }}
+            );
         }};
 
         impulseNode = new ImpulseNode("impulse-node"){{
