4dd09ef6416b3a61e2602663d2b3de03c2b82dce
= <uujuju41@gmail.com>
2024-07-25 15:42:51 -0300

    glasmore planet changes

--- src/omaloon/maps/ColorPass.java
+++ src/omaloon/maps/ColorPass.java
@@ -10,68 +10,91 @@ public abstract class ColorPass {
 	public abstract @Nullable Color color(Vec3 pos, float height);
 
 	/**
-	 * A pass that fills regions inside a sphere for use in craters
-	 * @see omaloon.maps.HeightPass.CraterHeight CraterHeight
+	 * A pass that paints points inside a sphere.
+	 * @see HeightPass.SphereHeight SphereHeight
 	 */
-	public static class CraterColorPass extends ColorPass {
-		public Vec3 position;
+	public static class SphereColorPass extends ColorPass {
+		/**
+		 * Position of the sphere relative to the planet.
+		 */
+		public Vec3 pos;
+		/**
+		 * Radius of the sphere.
+		 */
 		public float radius;
+		/**
+		 * Color painted inside the sphere.
+		 */
 		public Color out;
 
-		public CraterColorPass(Vec3 position, float radius, Color out) {
-			this.position = position;
+		public SphereColorPass(Vec3 pos, float radius, Color out) {
+			this.pos = pos;
 			this.radius = radius;
 			this.out = out;
 		}
 
 		@Override
 		public Color color(Vec3 pos, float height) {
-			if (pos.dst(position) < radius) return out;
+			if (pos.dst(this.pos) < radius) return out;
 			return null;
 		}
 	}
 	/**
-	 * A pass that uses noise to fill regions on the planet
-	 * @see omaloon.maps.HeightPass.NoiseHeight NoiseHeight
+	 * A pass that uses noise to fill regions on the planet.
+	 * @see HeightPass.NoiseHeight NoiseHeight
 	 */
 	public static class NoiseColorPass extends ColorPass {
 		/**
-		 * noise parameters
+		 * Offset for the noise sample relative to the planet. Values far away from the origin are reccomended.
+		 */
+		public Vec3 offset = new Vec3();
+		/**
+		 * Noise seed.
 		 */
 		public int seed;
-		public double octaves = 1.0, persistence = 1.0, scale = 1.0;
+		/**
+		 * The amount of octves added to the noise.
+		 */
+		public double octaves = 1.0;
+		/**
+		 * Intensity multiplier for each octave.
+		 */
+		public double persistence = 1.0;
+		/**
+		 * Noise scale.
+		 */
+		public double scale = 1.0;
+		/**
+		 * Noise magnitude.
+		 */
 		public float magnitude = 1;
-		public Vec3 offset = new Vec3();
-
-		public float minNoise = 0f, maxNoise = 1f;
-		public float minHeight = -10f, maxHeight = 10f;
-
+		/**
+		 * Min and max treshold values of noise that paint will apply
+		 */
+		public float min = 0f, max = 1f;
+		/**
+		 * Color painted based on the noise.
+		 */
 		public Color out = Color.white;
 
 		@Override
 		public Color color(Vec3 pos, float height) {
-			float noise = Simplex.noise3d(
-				seed,
-				octaves, persistence, scale,
-				pos.x + offset.x, pos.y + offset.y, pos.z + offset.z
-			) * magnitude;
-			if (
-				(minNoise <= noise && noise <= maxNoise) &&
-				(minHeight <= height && height <= maxHeight)
-			) return out;
+			float noise = Simplex.noise3d(seed, octaves, persistence, scale, pos.x + offset.x, pos.y + offset.y, pos.z + offset.z) * magnitude;
+			if (min <= noise && noise <= max) return out;
 			return null;
 		}
 	}
 	/**
-	 * A pass that paints regions whose height is within a boundary
+	 * A pass that paints regions whose height is within a boundary.
+	 * @see HeightPass.ClampHeight ClampHeight
 	 */
 	public static class FlatColorPass extends ColorPass {
-		public float minHeight = 0f, maxHeight = 1f;
+		public float min = 0f, max = 1f;
 		public Color out = Color.white;
 
 		@Override
 		public Color color(Vec3 pos, float height) {
-			if (minHeight < height && height < maxHeight) return out;
+			if (min <= height && height <= max) return out;
 			return null;
 		}
 	}
--- src/omaloon/maps/HeightPass.java
+++ src/omaloon/maps/HeightPass.java
@@ -11,47 +11,73 @@ public abstract class HeightPass {
 	public abstract float height(Vec3 pos, float height);
 
 	/**
-	 * A pass for creating craters defined by a sphere.
-	 * offset sets the offset that the crater creates.
-	 * set defines if the offset increments or sets the height of the mesh.
+	 * A pass that affects points inside a sphere.
 	 */
-	public static class CraterHeight extends HeightPass {
-		public Vec3 position;
-		public float radius, offset;
+	public static class SphereHeight extends HeightPass {
+		/**
+		 * Position of the sphere relative to the planet.
+		 */
+		public Vec3 pos = new Vec3();
+		/**
+		 * Radius of the sphere.
+		 */
+		public float radius = 0f;
+		/**
+		 * Height offset applied inside the sphere
+		 */
+		public float offset = 0f;
+		/**
+		 * When true, this pass will set the current height to the offset instead of increasing the height by the offset.
+		 */
 		public boolean set = false;
-
-		public CraterHeight(Vec3 position, float radius, float offset) {
-			this.position = position;
-			this.radius = radius;
-			this.offset = offset;
-		}
 
 		@Override
 		public float height(Vec3 pos, float height) {
-			if (pos.dst(position) < radius) return offset + height * (set ? 0f : 1f);
+			if (pos.dst(this.pos) < radius) return offset + height * (set ? 0f : 1f);
 			return height;
 		}
 	}
 	/**
-	 * A pass for creating a noisy terrain. That uses simplex Noise.
-	 * offset is a noise position offset.
-	 * heightOffset is the noise result offset.
+	 * A pass that affects points based on noise.
 	 * @see Simplex
 	 */
 	public static class NoiseHeight extends HeightPass {
+		/**
+		 * Offset for the noise sample relative to the planet. Values far away from the origin are reccomended.
+		 */
 		public Vec3 offset = new Vec3();
+		/**
+		 * Noise seed.
+		 */
 		public int seed;
-		public double octaves = 1.0, persistence = 1.0, scale = 1.0;
-		public float magnitude = 1, heightOffset = 0;
+		/**
+		 * The amount of octves added to the noise.
+		 */
+		public double octaves = 1.0;
+		/**
+		 * Intensity multiplier for each octave.
+		 */
+		public double persistence = 1.0;
+		/**
+		 * Noise scale.
+		 */
+		public double scale = 1.0;
+		/**
+		 * Noise magnitude.
+		 */
+		public float magnitude = 1;
+		/**
+		 * Value offset applied to the noise result.
+		 */
+		public float heightOffset = 0;
 
 		@Override
 		public float height(Vec3 pos, float height) {
-			pos = new Vec3(pos).add(offset);
-			return Simplex.noise3d(seed, octaves, persistence, scale, pos.x, pos.y, pos.z) * magnitude + heightOffset + height;
+			return Simplex.noise3d(seed, octaves, persistence, scale, pos.x + offset.x, pos.y + offset.y, pos.z + offset.z) * magnitude + heightOffset + height;
 		}
 	}
 	/**
-	 * A pass for clamping the height between 2 values.
+	 * A pass that clamps the current point's height between a min and max value.
 	 */
 	public static class ClampHeight extends HeightPass {
 		public float min, max;
@@ -67,16 +93,40 @@ public abstract class HeightPass {
 		}
 	}
 	/**
-	 * uses the angle relative to a certain direction as input to an interp function.
+	 * A pass that affects points based on it's dot product in relation to a direction.
 	 */
-	public static class AngleInterpHeight extends HeightPass {
+	public static class DotHeight extends HeightPass {
+		/**
+		 * Main direction vector. Is normalized later.
+		 */
 		public Vec3 dir = new Vec3();
+
+		/**
+		 * Min and max dot result where this pass applies.
+		 */
+		public float min = -1f;
+		public float max = 1f;
+
+		/**
+		 * When true, the dot result will be mapped from min to max instead of -1 to 1.
+		 */
+		public boolean map = true;
+
+		/**
+		 * Interpolation curve applied to the mapped dot result.
+		 */
 		public Interp interp = Interp.linear;
+		/**
+		 * Magnitude applied to the final height offset.
+		 */
 		public float magnitude = 1;
 
 		@Override
 		public float height(Vec3 pos, float height) {
-			return interp.apply(1f - pos.angle(dir)/180f) * magnitude + height;
+			float dot = dir.nor().dot(pos);
+			if (dot < min || dot > max) return height;
+			dot = Mathf.map(dot, map ? min : -1f, map ? max : 1f, 0f, 1f);
+			return interp.apply(dot) * magnitude + height;
 		}
 	}
 }
