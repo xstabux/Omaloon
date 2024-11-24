9163a83049ab6cba91a681d0332c51fd0097c779
xstabux <buksta@outlook.com>
2024-11-24 19:11:18 +0200

    Update bundles

--- src/omaloon/OmaloonMod.java
+++ src/omaloon/OmaloonMod.java
@@ -21,7 +21,6 @@ import omaloon.world.blocks.environment.*;
 import static arc.Core.*;
 
 public class OmaloonMod extends Mod{
-    public static Mods.LoadedMod modInfo;
     public static EditorListener editorListener;
     public static SafeClearer safeClearer;
 
